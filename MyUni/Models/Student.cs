fe3944c4420bae213f07d9a75bd6645456b4f5a7
Muhammad Shaharyar <ac149815@avcol.school.nz>
2023-05-17 12:35:35 +1200

    added fullname

--- MyUni/Models/Student.cs
+++ MyUni/Models/Student.cs
@@ -1,10 +1,15 @@
-﻿namespace MyUni.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyUni.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+        
         public DateTime EnrollmentDate { get; set; }
         public DateTime DOB { get; set; }
         public ICollection<Enrollment>Enrollments { get; set; }
